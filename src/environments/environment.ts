9e31d616fbbfaaaf99da94f4829e91c729e89ba2
Sabaniki <sawada13@mccc.jp>
2021-01-10 00:44:10 +0900

    firebaseConfigを追加し、Firebase使うために必要なモジュールをimportした

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,14 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCd17MV0EujR5kNu25uz7F_RH1r8tdbjDs',
+  authDomain: 'linkpacket.firebaseapp.com',
+  projectId: 'linkpacket',
+  storageBucket: 'linkpacket.appspot.com',
+  messagingSenderId: '527170168300',
+  appId: '1:527170168300:web:94f41f89652e9999c90cc9',
+  measurementId: 'G-TTJDH8RS30'
+};
