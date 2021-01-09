9c36fdbebb49831b0aebd6ba552ae51e6395e993
rytaryu <53173674+rytar@users.noreply.github.com>
2021-01-10 01:15:47 +0900

    華麗な衝突回避？

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/styles/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AngularFireModule} from '@angular/fire';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 import {firebaseConfig} from '../forFirebase/config';
+import { HeaderComponent } from './components/styles/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -19,11 +19,11 @@ import {firebaseConfig} from '../forFirebase/config';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
-    BrowserAnimationsModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFirestoreModule,
     AngularFireAuthModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
