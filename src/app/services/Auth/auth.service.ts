dea783df47572c3e9571a97b2f090637fa2d2c75
Sabaniki <sawada13@mccc.jp>
2021-01-10 22:14:18 +0900

    おそらくこれでログインページの実装はできるはず

---
+++ src/app/services/Auth/auth.service.ts
@@ -0,0 +1,53 @@
+import {Injectable} from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {UserDbService} from '../UserDb/user-db.service';
+// import {Router} from '@angular/router';  ←guard作るときに使う
+import {Observable, of} from 'rxjs';
+import {User} from '../../models/user';
+import {switchMap} from 'rxjs/operators';
+// @ts-ignore
+import {auth} from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class AuthService {
+  user$: Observable<User>;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private userDbService: UserDbService,
+    // private router: Router  ←guard作るときに使う
+  ) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.userDbService.getUserFromUid(user.uid);
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
+
+  async googleSignin(): Promise<void> {
+    const provider = new auth.GoogleAuthProvider();
+    const credential = await this.afAuth.signInWithPopup(provider);
+    return this.updateUserData(credential.user);
+  }
+
+  private updateUserData(user: User): Promise<void> {
+    // Sets user data to firestore on login
+    const userRef = this.userDbService.getUserRefFromUid(user.uid);
+
+    const data: User = {
+      uid: user.uid,
+      bookMarks: user.bookMarks || null,
+      displayName: user.displayName || ''
+    };
+
+    return userRef.set(data, {merge: true});
+  }
+}
---
+++ src/app/services/UserDb/user-db.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {AngularFirestore, AngularFirestoreDocument} from '@angular/fire/firestore';
+import {User} from '../../models/user';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDbService {
+
+  constructor(private afs: AngularFirestore) {
+  }
+
+  getUserFromUid(uid: string): Observable<User> {
+    return this.afs.doc<User>(`users/${uid}`).valueChanges();
+  }
+
+  getUserRefFromUid(uid: string): AngularFirestoreDocument<User> {
+    return this.afs.doc<User>(`users/${uid}`);
+  }
+
+}
+
