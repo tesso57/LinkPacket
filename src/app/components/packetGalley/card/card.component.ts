02978946b5a619bdaed213c4e85baae9e949eb78
tesso <ta4780@icloud.com>
2021-01-11 01:35:04 +0900

    カードのコンポーネントにプロパティを追加

--- src/app/components/packetGalley/card/card.component.ts
+++ src/app/components/packetGalley/card/card.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  @Input() title: string;
+  @Input() description: string;
+  @Input() url: string;
 
   public img: string;
   constructor() { }
 
   ngOnInit(): void {
     const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
-    const url = CORS_PROXY + 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494';
 
-    this.getOGPImage(url);
+    this.getOGPImage(CORS_PROXY + this.url); // 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494' WIP
   }
 
   private getOGPImage(url: string){
