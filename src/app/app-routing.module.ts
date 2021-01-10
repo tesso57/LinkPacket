0c19709fbc7040cf7824e91532ecf1a0cbb26f8a
rytaryu <53173674+rytar@users.noreply.github.com>
2021-01-10 22:14:32 +0900

    ルーティングのとこいじった

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [{path: '', component: PageNotFoundComponent}, {path: 'login', component: LoginComponent}];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
