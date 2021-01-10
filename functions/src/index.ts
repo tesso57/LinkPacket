a11aba9378341760a286d9fe42c4409d46b2fe2d
Sabaniki <sawada13@mccc.jp>
2021-01-11 01:20:31 +0900

    ユーザIDを渡すとブックマークがもらえるFunctionができました

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,21 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import {User} from '../../src/app/models/user'; // eslint-disable-line
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', {structuredData: true});
-  response.send('Hello from Firebase!');
-});
+export const getBookMarkFromUserId =
+  functions.https.onRequest(async (request, response) => {
+    const userId: unknown = request.query;
+    functions.logger.info('userId: ' + userId, {structuredData: true});
+    const bookMark = await admin.firestore()
+        .collection('users')
+        .doc(userId as string)
+        .get()
+        .then((doc) => {
+          const user = doc.data() as User;
+          return user.bookMarks;
+        });
+    response.json({result: bookMark});
+  });
